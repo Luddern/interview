e52b30a091c02e483de5fa7eaf3a2b8f42ef532b
Ludde <t66220092@gmail.com>
2023-02-07 15:22:45 +0800

    first version

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+var ShopCart= document.getElementById('shopCartBox');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    if (event.target == ShopCart) {
+        $("#shopCartBox").fadeOut("slow");
+        console.log(999)
+        
+    }
+}
+})
+
+function login(){
+    $('#id01').css("display","block")
+    console.log('123')
+
+}
+
+function qrInfo(){
+    $('#id02').css("display","block")
+    console.log('qrcode')
+
+}
+
+
+function showCart(){
+    $('#shopCartBox').css("display","block")
+    console.log('CART')
+
+}
